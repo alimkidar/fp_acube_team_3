392833932e9c755fdba28a081b0c078cb065dd57
Alim KH <alim.kidh11@gmail.com>
2019-09-30 23:09:49 +0700

    add forecast functions

---
+++ demand_forecast_functions.py
@@ -0,0 +1,55 @@
+from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.api import SimpleExpSmoothing
+from statsmodels.tsa.api import Holt
+from statsmodels.tsa.api import ExponentialSmoothing
+from statsmodels.tsa.api import SARIMAX
+from google.cloud import bigquery
+import pandas as pd
+import numpy as np
+import warnings
+import math
+
+
+warnings.filterwarnings("ignore")
+
+def init():
+    df = pd.read_gbq("""SELECT * FROM alim_hanif.tab_actual""", project_id='minerva-da-coe', dialect='standard')
+    return df
+
+def get_data(df, cluster, warehouse):
+    df = df[(df['Cluster'] == cluster)&(df['Warehouse'] == warehouse)]
+    df = df.sort_values('Week').set_index('Week')
+    return df
+
+def MA_f(df, p):
+    return df['Actual'].tail(p).mean()
+def SES_f(df, a):
+    simpleexp = SimpleExpSmoothing(np.array(np.array(df['Actual'])))
+    fit_simpleexp = simpleexp.fit(smoothing_level=a,optimized=False)
+    forecast = fit_simpleexp.forecast()
+    return forecast[0]
+def DEF_f(df, alpha, beta):
+    double_exp = Holt(np.array(df['Actual']), exponential=True)
+    fit_double_exp = double_exp.fit(smoothing_level=alpha, smoothing_slope=beta,optimized=False)
+    forecast = fit_double_exp.forecast()
+    return forecast[0]
+def DEF_damping_f(df, alpha, beta):
+    double_exp = Holt(np.array(df['Actual']), exponential=True, damped=True)
+    fit_double_exp = double_exp.fit(smoothing_level=alpha, smoothing_slope=beta,optimized=False)
+    forecast = fit_double_exp.forecast()
+    return forecast[0]
+def holt_winter_f(df, trend, seasonal):
+    holt_w = ExponentialSmoothing(np.array(df['Actual']), trend=trend, seasonal=seasonal, seasonal_periods=4)
+    fit_holt_w = holt_w.fit(use_boxcox=True)
+    forecast = fit_holt_w.forecast()
+    return forecast[0]
+def ARIMA_forecast(df, p, d, q):
+    arima_mod = ARIMA(np.array(df['Actual']), order=[p,d,q])
+    fit_arima = arima_mod.fit(use_boxcox=True)
+    forecast = fit_arima.forecast()
+    return forecast[0][0]
+def SARIMA_f(df, pdq, s):
+    sarima_mod = SARIMAX(np.array(df['Actual']), order=pdq, seasonal_order=s)
+    fit_sarima = sarima_mod.fit(use_boxcox=True)
+    forecast = fit_sarima.forecast()
+    return forecast[0]
---
+++ update_forecast.py
@@ -0,0 +1,6 @@
+from demand_forecast_functions import *
+
+import pandas as pd 
+import numpy as np 
+
+from google.cloud import bigquery
